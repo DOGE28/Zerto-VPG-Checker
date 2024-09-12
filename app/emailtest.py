ba13201c0412b74ec2dfcfcfef42ba2bfd2d614c
Taylor <tsully28@hotmail.com>
2024-09-11 18:12:05 -0600

    Major refactor for Linux ZVM

--- app/emailtest.py
+++
@@ -1,6 +0,0 @@
-import emailer
-
-em = emailer.Email()
-
-email = em.create_email("This is a test email.")
-em.send_email(email)
---
+++ appv2/alerts.py
@@ -0,0 +1,19 @@
+import zerto as z
+
+
+
+
+
+
+
+class Alerts():
+
+    def __init__(self):
+        pass
+
+    def determine(self):
+        pass
+
+    def send_alert(self):
+        pass
+
